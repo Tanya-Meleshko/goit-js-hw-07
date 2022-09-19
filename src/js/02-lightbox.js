9040e58263f5aed57b5a47fb4cb4f8699c03a8d7
Tanya-Meleshko <melehhha@gmail.com>
2022-09-19 19:31:24 +0300

    Task 2 solution

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryUlEl = document.querySelector('.gallery');
+
+const markupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join('');
+
+galleryUlEl.insertAdjacentHTML('beforeend', markupGallery);
+
+new SimpleLightbox('.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
